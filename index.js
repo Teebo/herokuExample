07b328df08b16270198f5ffc82e08847625c6937
Thabo <thabo.ngubane@primafin.co.za>
2019-02-01 16:29:55 +0200

    Add EJS  style signin

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .get('/', (req, res) => res.render('index.html'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
