cbe39a059fec3f6b6d6ae5ffdb73599d6f44d350
Thabo <thabo.ngubane@primafin.co.za>
2019-02-01 15:55:31 +0200

    Add index HTML

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .get('/', (req, res) => res.send('Hola heroku'))
+  .get('/', (req, res) => res.render('index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
